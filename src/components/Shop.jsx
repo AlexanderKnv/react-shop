9a4e316c1ca7857c29583b59994fe998e456425b
Konaschkov <konashkov23@gmail.com>
2023-10-12 21:55:04 +0200

    Add Alert

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -4,12 +4,14 @@ import { Preloader } from './Preloader';
 import { GoodsList } from './GoodsList';
 import { Cart } from './Cart';
 import { BasketList } from './BasketList';
+import { Alert } from './Alert';
 
 function Shop() {
     const [goods, setGoods] = useState([]);
     const [loading, setLoading] = useState(true);
     const [order, setOrder] = useState([]);
     const [isBasketShow, setBasketSow] = useState(false);
+    const [alertName, setAlertName] = useState('');
 
     const addToBasket = (item) => {
         const itemIndex = order.findIndex(
@@ -36,6 +38,7 @@ function Shop() {
 
             setOrder(newOrder);
         }
+        setAlertName(item.name);
     };
 
     const removeFromBasket = (itemId) => {
@@ -76,6 +79,10 @@ function Shop() {
         setOrder(newOrder);
     };
 
+    const closeAlert = () => {
+        setAlertName('');
+    };
+
     useEffect(function getGoods() {
         fetch(API_URL, {
             headers: {
@@ -101,6 +108,7 @@ function Shop() {
                     decQuantity={decQuantity}
                 />
             )}
+            {alertName && <Alert name={alertName} closeAlert={closeAlert} />}
     </main>
 }
 
